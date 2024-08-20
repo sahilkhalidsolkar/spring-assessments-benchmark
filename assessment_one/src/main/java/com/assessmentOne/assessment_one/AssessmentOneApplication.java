afc359806eb86193686f50802f25cacb402328d7
Sahil Khalid Solkar <sahilksolkar20@gmail.com>
2024-08-20 18:59:52 +0530

    Run on default port 8080

---
+++ assessment_one/src/main/java/com/assessmentOne/assessment_one/AssessmentOneApplication.java
@@ -0,0 +1,13 @@
+package com.assessmentOne.assessment_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentOneApplication.class, args);
+	}
+
+}
